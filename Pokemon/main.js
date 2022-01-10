c2c3ce233736cff85029d92ca4e2c9ef3e9f94a8
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2022-01-10 20:22:06 -0300

    Pokemon playground - Primer commit

---
+++ Pokemon/main.js
@@ -0,0 +1,312 @@
+var pokemones = [];
+
+var offset = 1;
+var limit = 11;
+var accorVerMas;  
+
+const typeColors = {
+    electric: '#FFEA70',
+    normal: '#B09398',
+    fire: '#FF675C',
+    water: '#0596C7',
+    ice: '#AFEAFD',
+    rock: '#999799',
+    flying: '#7AE7C7',
+    grass: '#4A9681',
+    psychic: '#FFC6D9',
+    ghost: '#561D25',
+    bug: '#A2FAA3',
+    poison: '#795663',
+    ground: '#D2B074',
+    dragon: '#DA627D',
+    steel: '#1D8A99',
+    fighting: '#8c3eb1',
+    default: '#2A1A1F',
+};
+
+//Cambia el contenido del accordion.
+if(document.getElementById("centrar")!=undefined)
+{
+    accorVerMas = document.getElementById("centrar");  
+    let botonVerMas = document.getElementById("btn-ver-mas"); 
+    botonVerMas.addEventListener("click", ()=>{
+        if(botonVerMas.textContent.includes("Ver Todos"))
+        {
+            botonVerMas.textContent="Ver Menos";
+        }
+        else if(botonVerMas.textContent.includes("Ver Menos"))
+        {
+            botonVerMas.textContent="Ver Todos";
+        }
+    })
+}
+
+//Funcion que optiene un pokemon.
+const getPokemon = async  (id) =>
+{
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const pokemon = await response.json();
+    pokemones.push(pokemon);
+    return pokemon;
+}
+
+//completa las options del select que se utiliza para el filtro de tipos.
+function completarTipos(allPokemones)
+{
+   let arrayTipos = [];
+   for(let i=0; i<allPokemones.length;i++)
+    { 
+      if(!arrayTipos.includes(allPokemones[i].types[0].type.name))
+      {
+        arrayTipos.push(allPokemones[i].types[0].type.name);     
+      }
+    }
+
+   arrayTipos.sort();
+
+   if(document.getElementById("selectTipo")!=undefined)
+   {
+      for(let i=0; i<arrayTipos.length;i++)
+        { 
+          let tipo = arrayTipos[i];
+          let option = document.createElement("option");
+          option.className = "text-center m-0";
+          option.value =  `${arrayTipos[i]}`
+          option.innerHTML = `${tipo.charAt(0).toUpperCase() + tipo.slice(1)}`;
+          document.getElementById("selectTipo").appendChild(option);
+        }
+   }
+}
+
+//busca un pokemon por nombre o id. 
+async function searchPokemon(event)
+{
+    event.preventDefault();
+    let value = event.target.inpPokemon.value.toLowerCase();
+    if(value == '')
+    {
+        obtenerPrimerGeneracion();
+    }
+    else{
+        showSearch(await getPokemon(value.toLowerCase()));
+    }
+    
+}
+
+//obtiene e imprime la primer generacion de pokemon (los cuales son 151)
+const obtenerPrimerGeneracion = async  () =>
+{
+     let data;
+     let allPokemon = [];
+     let j = 0;
+    if(document.getElementById("contenedor")!=undefined)
+    {
+        document.getElementById("body-acor").innerHTML = ``;
+        accorVerMas.style.display="none";
+        let tipoPokemon = document.getElementsByName("tipos");
+        let tipo = 0;
+        tipoPokemon.forEach(element => {
+                if(element.selected)
+                {
+                    tipo=0;
+                }
+                else if(element.selectedIndex>0)
+                    {                        
+                            tipo=element.value;                                   
+                    }        
+                });
+
+        document.getElementById("contenedor").innerHTML = `<div class="spinner-border text-warning" id="cargando" role="status"> </div>`;
+        for (let i = 1; i < 152; i++) 
+        {
+        data = await getPokemon(i);
+        allPokemon.push(data);
+        }
+        document.getElementById("contenedor").innerHTML = ``;
+
+        allPokemon.forEach(pokemon =>{
+            j++;   
+             if(tipo== 0)
+                 {
+                    accorVerMas.style.display="flex";
+                    if(j<11)
+                    {
+                    imprimirPokemon("contenedor",pokemon);
+                    }  
+                    else
+                    {
+                    imprimirPokemon("body-acor",pokemon);
+                    }
+                 }
+            else if(pokemon.types[0].type.name === tipo)
+                {
+                    accorVerMas.style.display="none";
+                     imprimirPokemon("contenedor",pokemon);
+                }  
+            
+        } );
+        completarTipos(allPokemon);
+    }
+    
+}
+
+//Imprime un pokemon en el lugar que se el indique. 
+function imprimirPokemon(idHtml ,data)
+{
+    let nombre = data.forms[0].name.toUpperCase();
+    let imagen = data.sprites.other.dream_world.front_default;
+    
+
+    let id = data.id;
+    let tipo = data.types[0].type.name.toUpperCase();
+    let pokemon = document.createElement('div');
+    pokemon.className = "card col-2 m-4";
+    pokemon.innerHTML = `
+        <p class="card-text pokemon-id col-4 text-center">ID: ${id}</p>
+        <img src="${imagen}" class="card-img-top imgCard" name="${data.types[0].type.name}" style="filter: drop-shadow(0 5px 15px ${typeColors[data.types[0].type.name]}); ">
+        <div class="card-body">
+        <h5 class="card-title">${nombre}</h5>
+        
+        <p class="card-text" name="tipo">Type: ${tipo}</p>
+        <a href="./show_pokemon.html?idpoke=${data.id}" class="btn btn-link" target="_blank" id="${nombre}">See more...</a>
+        </div>
+    `;
+
+    if(document.getElementById(idHtml)!= undefined)
+    {
+        document.getElementById(idHtml).appendChild(pokemon); 
+    }
+
+    return data;
+}
+
+
+async function obtenerSeeMorePokemon()
+{ 
+    let pokemonName = location.href;
+    pokemonName = pokemonName.split('=');
+    pokemonName = pokemonName[1];  
+    let data = await getPokemon(pokemonName);
+    show(data);
+    let imagen = document.getElementById("img-mostrada");
+    let titulo = document.getElementById("titulo-mostrado");
+    const pag1 = document.getElementById("model-1");
+    const pag2 = document.getElementById("model-2");
+    const pag3 = document.getElementById("model-3");
+   
+
+    pag1.addEventListener("click", () => {
+        imagen.src= data.sprites.other.dream_world.front_default;
+        titulo.textContent = "MODEL 1";
+      });
+      
+      pag2.addEventListener("click", () => {
+        imagen.src= data.sprites.other.home.front_default;
+        titulo.textContent = "MODEL 2";
+      });
+    
+      pag3.addEventListener("click", () => {
+        imagen.src= data.sprites.other["official-artwork"].front_default;
+        titulo.textContent = "MODEL 3";
+      });
+
+}
+
+function show(data)
+{
+    let nombre = data.forms[0].name.toUpperCase();
+    let imagen = [];
+    imagen[0] = data.sprites.other.dream_world.front_default;
+    imagen[1] = data.sprites.other.home.front_default;
+    imagen[2] = data.sprites.other["official-artwork"].front_default;
+    imagen[3] = data.sprites.front_default;
+    let id = data.id;
+    let tipo = data.types[0].type.name.toUpperCase();
+    const { stats } = data;
+    
+    if(document.getElementById("showPokemon")!= undefined)
+    {
+        document.getElementById("showPokemon").innerHTML = `
+        <div class="col-6" id="stats-pokemon">
+            <p class="pokemon-id col-4 text-center p-2" id="id-font">ID: ${id}</p>
+            <h1 class="title" id="pokemon-title">${nombre}</h1>
+            <h4 id="" style="color: ${typeColors[data.types[0].type.name]};">Type: ${tipo}</h4>        
+        </div>
+
+        <div class="col-6 show-pokemon mt-3"> 
+
+        <h4 class="col-12 text-center"  id="titulo-mostrado">MODEL 1</h4>  
+        <img src="${imagen[0]}" class="col-12 imgPokemon" id="img-mostrada" style="filter: drop-shadow(0 5px 15px ${typeColors[data.types[0].type.name]}); "> 
+
+            <nav aria-label="Page navigation example">
+                <ul class="pagination display-flex justify-content-center mt-4 mb-0">
+                    <li id="model-1" class="page-item m-2"><a class="page-link btn-volver border-0" href="#">1</a></li>
+                    <li id="model-2" class="page-item m-2"><a class="page-link btn-volver border-0" href="#">2</a></li>
+                    <li id="model-3" class="page-item m-2"><a class="page-link btn-volver border-0" href="#">3</a></li>
+                </ul>
+            </nav>
+
+        </div>
+       
+    `; 
+        stats.forEach(stat => {
+        const statElement = document.createElement("div");
+        const statElementName = document.createElement("h4");
+        const statElementAmount = document.createElement("h4");
+        statElement.className = "text-center col-8 poke-stats";
+        statElementName.className = "text-center p-0";
+        statElementAmount.className = "text-center p-0"
+        statElementName.textContent = stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1);
+        statElementAmount.textContent = stat.base_stat;
+        statElement.appendChild(statElementName);
+        statElement.appendChild(statElementAmount);
+        document.getElementById("stats-pokemon").appendChild(statElement);
+    });
+    }
+
+
+}
+
+function showSearch(data)
+{
+    let accorVerMas = document.getElementById("centrar");  
+    let boton = document.createElement('div');
+    boton.className = "text-center m-4";
+    boton.innerHTML =`<a href="#" class="col-2 btn btn-volver" onclick="obtenerPrimerGeneracion()"><<< Volver</a>`;
+    
+    
+    if(document.getElementById("contenedor")!= undefined)
+    {
+        document.getElementById("contenedor").innerHTML = ``;
+        imprimirPokemon("contenedor",data);
+        document.getElementById("contenedor").appendChild(boton);
+        accorVerMas.style.display="none";
+    }
+}
+
+obtenerPrimerGeneracion();
+
+if(document.title=="show")
+{
+    obtenerSeeMorePokemon();
+}
+
+Vue.component('todo-footer', {
+    props: ['texto', 'link'],
+    template: `
+    <div class="container-fluid text-center p-3">
+          {{ texto }}
+          <a href="https://pokeapi.co">{{ link }}</a>
+        </div>
+    `
+  })
+  
+  var footer = new Vue({
+    el: '.footer',
+    data: { 
+    textos: [
+        {texto:'Lucio Andres Gonzalez Pokedex ©2021 Copyright ', link:' Pokeapi.co'},
+    ]
+        
+    }
+  });

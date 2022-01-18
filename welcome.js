494dee1234a0c16014243a5c02140b39c36bd718
lucio1500 <60608529+lucio1500@users.noreply.github.com>
2022-01-18 19:01:00 -0300

    Borrado de archivos

--- welcome.js
+++
@@ -1,7 +0,0 @@
-function cargaNombre(event)
-{
-    localStorage.clear();
-    // event.preventDefault();
-    let nombre = document.getElementById("loadName").value;
-    localStorage.setItem("nombre",nombre);
-}
